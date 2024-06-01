60d451ca354b89f3d95524a5a45260ea515de7f6
JagadishKumarM <140534302+JagadishKumarM@users.noreply.github.com>
2024-06-01 15:22:40 +0530

    Add files via upload
    The code showcases the queries used to create and work on the database with the data having been uploaded via web scraping.

---
+++ report.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS report;
+USE report;
+
+/* This statement disables the SQL safe updates mode, allowing bulk updates or deletes.*/
+SET sql_safe_updates=0;
+
+DESC TABLE cities;
+DESC TABLE forecast;
+
+SELECT * FROM cities;
+SELECT * FROM forecast;
+
+SELECT * 
+FROM Cities AS C 
+INNER JOIN Forecast AS F 
+ON C.City_Name=F.City_Name;
+
+
+/* Writing a query to select City names with elevation more than 300m and the temperature < 40 C on a particluar day */
+
+SELECT C.City_Name, C.Elevation_in_M, F.Temperature,F.`Time`
+FROM Cities C, Forecast F
+WHERE C.Elevation_in_M>300 AND F.Temperature <40 AND Day_of_Week='Thursday';
+
+
