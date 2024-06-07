68ef14639d657158c17e35fe830a045a0d41b433
JagadishKumarM <140534302+JagadishKumarM@users.noreply.github.com>
2024-06-07 15:42:06 +0530

    Update report.sql

--- report.sql
+++ report.sql
@@ -11,15 +11,15 @@ SELECT * FROM cities;
 SELECT * FROM forecast;
 
 SELECT * 
-FROM Cities AS C 
-INNER JOIN Forecast AS F 
+FROM cities AS C 
+INNER JOIN forecast AS F 
 ON C.City_Name=F.City_Name;
 
 
 /* Writing a query to select City names with elevation more than 300m and the temperature < 40 C on a particluar day */
 
 SELECT C.City_Name, C.Elevation_in_M, F.Temperature,F.`Time`
-FROM Cities C, Forecast F
+FROM cities C, forecast F
 WHERE C.Elevation_in_M>300 AND F.Temperature <40 AND Day_of_Week='Thursday';
 
 
